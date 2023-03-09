d932c73ba28a25c9c4b2ce5708e87133ae4d5469
virag <konkoly.t.virag@gmail.com>
2023-03-09 16:52:41 +0100

    Started HAZI02

--- HAZI/HAZI02/HAZI02.py
+++ HAZI/HAZI02/HAZI02.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import numpy as np
 
 
@@ -10,7 +12,7 @@ def column_swap(input_array: np.array) -> np.array:
     return return_array
 
 
-#Készíts egy olyan függvényt ami összehasonlít két array-t és adjon vissza egy array-ben, hogy hol egyenlőek
+# Készíts egy olyan függvényt ami összehasonlít két array-t és adjon vissza egy array-ben, hogy hol egyenlőek
 # Pl Be: [7,8,9], [9,8,7]
 # Ki: [1]
 # compare_two_array()
@@ -18,10 +20,10 @@ def column_swap(input_array: np.array) -> np.array:
 def compare_two_array(input_array1: np.array, input_array2: np.array) -> np.array:
     if input_array1.shape == input_array2.shape:
         return np.where(np.equal(input_array1, input_array2))[0]
-    return False
+    return np.array([])
 
 
-#Készíts egy olyan függvényt, ami vissza adja a megadott array dimenzióit:
+# Készíts egy olyan függvényt, ami vissza adja a megadott array dimenzióit:
 # Be: [[1,2,3], [4,5,6]]
 # Ki: "sor: 2, oszlop: 3, melyseg: 1"
 # get_array_shape()
@@ -34,4 +36,81 @@ def get_array_shape(input_array: np.array) -> str:
     else:
         return input_array.shape
 
-print(get_array_shape(np.array([[1,2,3], [4,5,6]])))
+
+# Készíts egy olyan függvényt, aminek segítségével elő tudod állítani egy neurális hálózat tanításához szükséges Y-okat egy numpy array-ből.
+# Bementként add meg az array-t, illetve hogy mennyi class-od van. Kimenetként pedig adjon vissza egy 2d array-t, ahol a sorok az egyes elemek. Minden nullákkal teli legyen és csak ott álljon egyes, ahol a bementi tömb megjelöli
+# Be: [1, 2, 0, 3], 4
+# Ki: [[0,1,0,0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]]
+# encode_Y()
+def encode_Y(input_array: np.array, n: int) -> np.array:
+    return_array = np.eye(n)[input_array].astype(int)
+    return return_array
+
+
+# A fenti feladatnak valósítsd meg a kiértékelését. Adj meg a 2d array-t és adj vissza a decodolt változatát
+# Be:  [[0,1,0,0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]]
+# Ki:  [1, 2, 0, 3]
+# decode_Y()
+def decode_Y(input_array: np.array) -> np.array:
+    return np.argmax(input_array, axis=1)
+
+
+# Készíts egy olyan függvényt, ami képes kiértékelni egy neurális háló eredményét! Bemenetként egy listát és egy array-t és adja vissza a legvalószínübb element a listából.
+# Be: ['alma', 'körte', 'szilva'], [0.2, 0.2, 0.6].
+# Ki: 'szilva'
+# eval_classification()
+def eval_classification(input_list: list, input_array: np.array):
+    return input_list[np.argmax(input_array)]
+
+# Készíts egy olyan függvényt, ahol az 1D array-ben a páratlan számokat -1-re cseréli
+# Be: [1,2,3,4,5,6]
+# Ki: [-1,2,-1,4,-1,6]
+# repalce_odd_numbers()
+def repalce_odd_numbers(input_array: np.array) -> np.array:
+    return np.where(input_array % 2 != 0, -1, input_array)
+
+
+# Készíts egy olyan függvényt, ami egy array értékeit -1 és 1-re változtatja, attól függően, hogy az adott elem nagyobb vagy kisebb a paraméterként megadott számnál.
+# Ha a szám kisebb mint a megadott érték, akkor -1, ha nagyobb vagy egyenlő, akkor pedig 1.
+# Be: [1, 2, 5, 0], 2
+# Ki: [-1, 1, 1, -1]
+# replace_by_value()
+def replace_by_value(input_array: np.array, input_value: int) -> np.array:
+    return np.where(input_array < input_value, -1, 1)
+
+
+# Készítsd egy olyan függvényt, ami az array értékeit összeszorozza és az eredmény visszaadja
+# Be: [1,2,3,4]
+# Ki: 24
+# array_multi()
+# Ha több dimenziós a tömb, akkor az egész tömb elemeinek szorzatával térjen vissza
+def array_multi(input_array: np.array) -> np.array:
+    return np.prod(input_array, axis=0)
+
+
+# Készítsd egy olyan függvényt, ami a 2D array értékeit összeszorozza és egy olyan array-el tér vissza, aminek az elemei a soroknak a szorzata
+# Be: [[1, 2], [3, 4]]
+# Ki: [2, 12]
+# array_multi_2d()
+def array_multi_2(input_array: np.array) -> np.array:
+    return np.product(input_array, axis=1)
+
+
+# Készíts egy olyan függvényt, amit egy meglévő numpy array-hez készít egy bordert nullásokkal. Bementként egy array-t várjon és kimenetként egy array jelenjen meg aminek van border-je
+# Be: [[1,2],[3,4]]
+# Ki: [[0,0,0,0],[0,1,2,0],[0,3,4,0],[0,0,0,0]]
+# add_border()
+def add_border(input_array: np.array) -> np.array:
+    return np.pad(input_array, ((1, 1), (1, 1)), mode='constant', constant_values=0)
+
+
+# Készíts egy olyan függvényt ami két dátum között felsorolja az összes napot.
+# Be: '2023-03', '2023-04'
+# Ki: ['2023-03-01', '2023-03-02', .. , '2023-03-31',]
+# list_days()
+def list_days(start_date: str, end_date: str) -> list:
+    start = datetime.strptime(start_date, '%Y-%m')
+    end = datetime.strptime(end_date, '%Y-%m')
+
+print(list_days(['2023-03', '2023-04']))
+
